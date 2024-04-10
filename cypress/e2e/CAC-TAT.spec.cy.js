3fbc1dea62a263c1cc90e7ecbd88ccf7bd60c782
Guto <guto.the.big@gmail.com>
2024-04-10 02:57:47 -0300

    .

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -103,7 +103,24 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   });
 
   //aula 4
+  //exercicio
+  it('marca o tipo de atendimento "Feedback"', () => {
+
+    cy.get('input[type="radio"][value="feedback"]').check().should('be.checked')
   
+  });
+
+  //exercicio extra
+  it.only('marca cada tipo de atendimento', () => {
+
+    cy.get('input[type="radio"]')
+      .should('have.length', 3)
+      .each(($radio)=>{
+        cy.wrap($radio).check()
+        cy.wrap($radio).should('be.checked')
+      })
+  });
+
 
 
 })
