6ffb0ec322d35fbac67021ec2dc43199833bad26
Guto <guto.the.big@gmail.com>
2024-04-10 02:08:17 -0300

    .

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   
-  viewportWidth: 1280,
-  viewportHeight: 880,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
 
   e2e: {
     setupNodeEvents(on, config) {
--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="Cypress" />
 
+
 describe('Central de Atendimento ao Cliente TAT', () => {
   beforeEach(()=>{
     cy.visit('../../src/index.html')
@@ -10,14 +11,15 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
   
   it('preenche os campos obrigatórios e envia formulário', ()=>{
+    const longText = 'teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
     cy.get('#firstName').type('Olá Mundo').should('have.value','Olá Mundo')
     cy.get('#lastName').type('Olá Mundo sobrenome').should('have.value','Olá Mundo sobrenome')
     cy.get('#email').type('ola.mundo@gmail.com').should('have.value','ola.mundo@gmail.com')
     cy.get('#open-text-area')
-    .type('gostaria de uma ajuda', {delay: 5})
-    .should('have.value','gostaria de uma ajuda')
+    .type(longText, {delay: 0})
+    .should('have.value',longText)
+    cy.get('button[type="submit"]').click()
 
-    cy.get('.button').click()
     cy.get('.success')
       .should('have.class','success')
       .should('be.visible')
@@ -29,11 +31,11 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').type('Olá Mundo sobrenome').should('have.value','Olá Mundo sobrenome')
     cy.get('#email').type('ola.mundo_gmail.com').should('have.value','ola.mundo_gmail.com')
     cy.get('#open-text-area')
-    .type('gostaria de uma ajuda', {delay: 10})
+    .type('gostaria de uma ajuda', {delay: 20})
     .should('have.value','gostaria de uma ajuda')
 
     cy.get('#white-background > form > button').click()
-    cy.get('body > span.error')
+    cy.get('.error')
       .should('have.class','error')
       .should('be.visible')
       .contains('Valide os campos obrigatórios!')
@@ -47,8 +49,11 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
   //exercicio 4
   it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Olá Mundo').should('have.value','Olá Mundo')
+    cy.get('#lastName').type('Olá Mundo sobrenome').should('have.value','Olá Mundo sobrenome')
+    cy.get('#email').type('ola.mundo@gmail.com').should('have.value','ola.mundo@gmail.com')
     cy.get('#phone-checkbox').check()
-    cy.get('.button').click()
+    cy.get('button[type="submit"]').click()
 
     cy.get('.error').should('be.visible')
   });
@@ -60,12 +65,16 @@ describe('Central de Atendimento ao Cliente TAT', () => {
  
   //exercicio 6
   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-    cy.get('#phone').type('99999999')
+    //cy.get('#phone').type('99999999')
     cy.get('.button').click()
     cy.get('.error').should('be.visible')
   });
 
   //exercicio 7
-  
+  it('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit() 
+    
+    cy.get('.sucess').should('be.visibles')
+  });
 
 })
