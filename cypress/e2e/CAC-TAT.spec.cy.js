eb967a888a8b03fa9d44eaeb1885adf3bb5d200f
Gutenberg de Souza <guto.the.big@gmail.com>
2024-04-09 21:32:28 -0300

    commit com algumas resoluções de exercios extras

--- cypress/e2e/CAC-TAT.spec.cy.js
+++ cypress/e2e/CAC-TAT.spec.cy.js
@@ -1,7 +1,5 @@
 /// <reference types="Cypress" />
 
-
-
 describe('Central de Atendimento ao Cliente TAT', () => {
   beforeEach(()=>{
     cy.visit('../../src/index.html')
@@ -11,16 +9,16 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
   })
   
-  it('preenche os campos obrigatórios e envia formulãrio', ()=>{
+  it('preenche os campos obrigatórios e envia formulário', ()=>{
     cy.get('#firstName').type('Olá Mundo').should('have.value','Olá Mundo')
     cy.get('#lastName').type('Olá Mundo sobrenome').should('have.value','Olá Mundo sobrenome')
     cy.get('#email').type('ola.mundo@gmail.com').should('have.value','ola.mundo@gmail.com')
     cy.get('#open-text-area')
-    .type('gostaria de uma ajuda', {delay: 0})
+    .type('gostaria de uma ajuda', {delay: 5})
     .should('have.value','gostaria de uma ajuda')
 
-    cy.get('#white-background > form > button').click()
-    cy.get('body > span.success')
+    cy.get('.button').click()
+    cy.get('.success')
       .should('have.class','success')
       .should('be.visible')
       .contains('Mensagem enviada com sucesso.')
@@ -41,10 +39,27 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .contains('Valide os campos obrigatórios!')
   });
 
-})
+  //exercicio 3
+  it('validar campo telefone', () => {
+    cy.get('#phone').type('qwertyuiopasdfghjklçzxcvbnm').should('have.value',"")
+    cy.get('#phone').type('qwertyuiopasdfghjklçzxcvbnm').should('be.empty')
+  });
 
-//exercicio 3 https://github.com/wlsf82/cypress-basico-v2/blob/main/lessons/02.md#exerc%C3%ADcio-extra-3
-it('validar campo telefone', () => {
-  
-  
-});
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#phone-checkbox').check()
+    cy.get('.button').click()
+
+    cy.get('.error').should('be.visible')
+  });
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName').type('teste teste teste').should('have.value','teste teste teste').clear().should('have.value','')
+  });
+ 
+  it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.get('#phone').type('99999999')
+    cy.get('.button').click()
+    cy.get('.error').should('be.visible')
+  });
+
+})
